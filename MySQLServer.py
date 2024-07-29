e579d844d1d9b36e9ed01cd482f58a73541e2c1b
jdam-github <jdam600@gmail.com>
2024-07-29 17:26:21 +0200

    initial commit

---
+++ MySQLServer.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        # Establish a connection to the MySQL server
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password=''
+        )
+        if connection.is_connected():
+            print("Connected to MySQL server")
+            cursor = connection.cursor()
+            
+            # Create the database if it does not exist
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+        
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+    
+    finally:
+        # Close the cursor and connection
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == "__main__":
+    create_database()
